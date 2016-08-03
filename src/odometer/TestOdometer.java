9c8801be7ae0546004ad4a0bf54623711a68b61f
Ashish Baghudana <ashish.baghudana26@gmail.com>
2016-08-03 18:17:50 +0530

    Added test for testPrevious and testNext

--- src/odometer/TestOdometer.java
+++ src/odometer/TestOdometer.java
@@ -1,5 +1,29 @@
 package odometer;
 
 public class TestOdometer {
-
+	public static boolean testPrevious() {
+		int digit = 1235;
+		int size = 4;
+		if (Odometer.previous(digit, size) != 1234) {
+			return false;
+		}
+		digit = 1234;
+		if (Odometer.previous(digit, size) != 6789) {
+			return false;
+		}
+		return true;
+	}
+	
+	public static boolean testNext() {
+		int digit = 1234;
+		int size = 4;
+		if (Odometer.next(digit, size) != 1235) {
+			return false;
+		}
+		digit = 6789;
+		if (Odometer.next(digit, size) != 1234) {
+			return false;
+		}
+		return true;
+	}
 }
