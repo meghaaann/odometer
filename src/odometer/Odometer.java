d02753d07fe2287c410541df64c74a88ca7b199d
unknown <meanil@L-MAA-25005052.paypalcorp.com>
2016-08-03 17:57:05 +0530

    added next function

--- src/odometer/Odometer.java
+++ src/odometer/Odometer.java
@@ -1,5 +1,7 @@
 package odometer;
 
+import java.util.Scanner;
+
 public class Odometer {
 	public static int previous(int digit, int size) {
 		do {
@@ -10,5 +12,29 @@ public class Odometer {
 		}
 		while (!isValid(digit, size))
 		return digit;
+	}
+	
+	public static int next(int digit,int size)
+	{
+		
+		int nextVal;
+		
+		while(isValid(digit))
+		{
+		nextVal=digit++;
+		
+		if((digit%10^(size+1))==1)
+		{
+			nextVal=1;
+		}
+		}
+		return nextVal;
+	}
+	
+	public static void main(String args[])
+	{
+		System.out.println("Enter the value");
+		Scanner reader = new Scanner(System.in); 
+		int digit=reader.nextInt();
 	}
 }
