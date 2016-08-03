a5774e22b5016db243288e36a6caca630c1057ec
Shivaram <sshivaram@paypalcorp.com>
2016-08-03 17:56:05 +0530

    Added valid and difference functions

--- src/odometer/Odometer.java
+++ src/odometer/Odometer.java
@@ -1,6 +1,26 @@
 package odometer;
 
 public class Odometer {
+	public static boolean isValid(int number, int size) {
+		int digit;
+		for(int i=0;i<size-1;i++) {
+			digit=number%10;
+			number=number/10;
+			if(digit<number%10)
+				return false;
+		}
+		return true;
+	}
+	
+	public static int differenceBetweenOdNumbers(int firstNum,int secondNum,int size) {
+		int count=0;
+		for(int i=firstNum;i<secondNum;i++){
+			if(isValid(i,size))
+				count++;
+		}
+		return count;
+	}
+	
 	public static int previous(int digit, int size) {
 		do {
 			digit--;
