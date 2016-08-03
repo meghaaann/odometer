6739604674fab543ac7526947c80c2dbbcaec5c1
Ashish Baghudana <ashish.baghudana26@gmail.com>
2016-08-03 18:33:45 +0530

    Fix bugs with next function

--- src/odometer/Odometer.java
+++ src/odometer/Odometer.java
@@ -3,53 +3,48 @@ package odometer;
 public class Odometer {
 	public static boolean isValid(int number, int size) {
 		int digit;
-		if (number < Math.pow(10, size-1) - 1) {
+		if (number < Math.pow(10, size - 1) - 1) {
 			return false;
 		}
-		for(int i=0;i<size-1;i++) {
-			digit = number%10;
-			number = number/10;
-			if(digit <= number%10)
+		for (int i = 0; i < size - 1; i++) {
+			digit = number % 10;
+			number = number / 10;
+			if (digit <= number % 10)
 				return false;
 		}
 		return true;
 	}
-	
-	public static int differenceBetweenOdNumbers(int firstNum,int secondNum,int size) {
-		int count=0;
-		for(int i=firstNum;i<secondNum;i++){
-			if(isValid(i,size))
+
+	public static int differenceBetweenOdNumbers(int firstNum, int secondNum, int size) {
+		int count = 0;
+		for (int i = firstNum; i < secondNum; i++) {
+			if (isValid(i, size))
 				count++;
 		}
 		return count;
 	}
-	
+
 	public static int previous(int digit, int size) {
 		do {
 			digit--;
-			if (digit<0) {
+			if (digit < 0) {
 				digit = (int) Math.pow(10, size) - 1;
 			}
-		}
-		while (!isValid(digit, size));
+		} while (!isValid(digit, size));
 		return digit;
 	}
-	
-	public static int next(int digit,int size) {
-		int nextVal = 0;
-		
-		while(isValid(digit,size))
-		{
-		nextVal=digit++;
-		
-		if((digit%10^(size+1))==1)
-		{
-			nextVal=1;
-		}
+
+	public static int next(int digit, int size) {
+		int nextVal = digit+1;
+		while (!isValid(nextVal, size)) {
+			if (nextVal >= Math.pow(10, size)) {
+				nextVal = 1;
+			}
+			nextVal++;
 		}
 		return nextVal;
 	}
-	
+
 	public static int nAfter(int digit, int n, int size) {
 		int counter = 0;
 		while (counter < n) {
@@ -57,7 +52,7 @@ public class Odometer {
 		}
 		return digit;
 	}
-	
+
 	public static int nBefore(int digit, int n, int size) {
 		int counter = 0;
 		while (counter < n) {
