d6dcd13a435df3f7c809fa54f6d2e1e375968cec
Shivaram <sshivaram@paypalcorp.com>
2016-08-03 18:28:12 +0530

    added test case

--- src/odometer/TestOdometer.java
+++ src/odometer/TestOdometer.java
@@ -26,4 +26,19 @@ public class TestOdometer {
 		}
 		return true;
 	}
+	
+	public static boolean testDifference() {
+		int digit1 = 1234;
+		int digit2=1237;
+		int size=4;
+		if( Odometer.differenceBetweenOdNumbers(digit1, digit2, size)!=3) {
+			return false;
+		}
+		digit1 = 1234;
+		digit2 = 1345;
+		if(Odometer.differenceBetweenOdNumbers(digit1, digit2, size)!=6) {
+			return false;
+		}
+		return true;
+	}
 }
